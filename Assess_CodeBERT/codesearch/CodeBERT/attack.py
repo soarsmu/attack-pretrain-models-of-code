ec4fa2ed93b776987f46174fadfd2d7e36259281
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-07-18 19:49:22 +0800

    feat(BC-Attack): initialize several functions

--- Assess_CodeBERT/codesearch/CodeBERT/attack.py
+++ Assess_CodeBERT/codesearch/CodeBERT/attack.py
@@ -6,6 +6,22 @@
 '''For attacking CodeBERT models'''
 
 import argparse
+import warnings
+import os
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning) # Only report warning
+
+
+def get_identifiers():
+    pass
+
+def get_masked_code():
+    pass
+
+def get_importance_score():
+    pass
 
 
 def main():
