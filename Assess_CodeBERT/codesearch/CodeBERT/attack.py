e91e5a7c88a6014464b1cb7e62e5880bc000032a
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-07-18 19:43:47 +0800

    feat(CB-Attack): initialize attack.py for CodeBERT

---
+++ Assess_CodeBERT/codesearch/CodeBERT/attack.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# @Time    : 2020/7/8
+# @Author  : Zhou Yang
+# @Email   : zyang@smu.edu.sg
+# @File    : attack.py
+'''For attacking CodeBERT models'''
+
+import argparse
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
