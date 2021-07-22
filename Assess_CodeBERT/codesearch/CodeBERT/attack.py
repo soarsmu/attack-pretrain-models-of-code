253c1bfa0a8d2927ad7c48815ab71bc6b88310a5
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-07-22 16:57:36 +0800

    feat(BC-ATTACK): load datasets

--- Assess_CodeBERT/codesearch/CodeBERT/attack.py
+++ Assess_CodeBERT/codesearch/CodeBERT/attack.py
@@ -13,8 +13,13 @@ from transformers import (WEIGHTS_NAME, get_linear_schedule_with_warmup, AdamW,
                           RobertaConfig,
                           RobertaForSequenceClassification,
                           RobertaTokenizer)
+from torch.utils.data import TensorDataset, SequentialSampler, DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
+from utils import convert_examples_to_features, CodesearchProcessor
+import torch
 from transformers import RobertaForMaskedLM, pipeline
+from tqdm import tqdm
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.simplefilter(action='ignore', category=FutureWarning) # Only report warning
@@ -42,6 +47,9 @@ def main():
     parser.add_argument("--use_bpe", type=int, )
     parser.add_argument("--k", type=int, )
     parser.add_argument("--threshold_pred_score", type=float, )
+    parser.add_argument("--max_seq_length", default=128, type=int,
+                        help="The maximum total input sequence length after tokenization. Sequences longer "
+                             "than this will be truncated, sequences shorter will be padded.")
 
     args = parser.parse_args()
     data_path = str(args.data_path)
@@ -53,12 +61,14 @@ def main():
     use_bpe = args.use_bpe
     k = args.k
     threshold_pred_score = args.threshold_pred_score
+    max_seq_length = args.max_seq_length
 
     ## ----------------Load Models------------------- ##
 
     ## Load CodeBERT (MLM) model
     codebert_mlm = RobertaForMaskedLM.from_pretrained("microsoft/codebert-base-mlm")
     tokenizer_mlm = RobertaTokenizer.from_pretrained("microsoft/codebert-base-mlm")
+    codebert_mlm.to('cuda')
 
     '''
     CodeBERT MLM Model Usage Example (from CodeBERT README file):
@@ -77,8 +87,51 @@ def main():
     config_tgt = RobertaConfig.from_pretrained(tgt_path, num_labels=num_labels, finetuning_task='codesearch')
     tokenizer_tgt = RobertaTokenizer.from_pretrained('roberta-base')
     codebert_tgt = RobertaForSequenceClassification.from_pretrained(tgt_path, config=config_tgt)
+    codebert_tgt.to('cuda')
 
-    ## 貌似这里有点问题
+    # TO-DO: 这里貌似有问题，具体看This IS NOT expected的那条消息
+
+    ## ----------------Load Datasets------------------- ##
+    processor = CodesearchProcessor()
+    label_list = processor.get_labels() # ['0', '1']
+    examples = processor.get_new_train_examples(data_path, "triple_train.txt")
+
+    print('this is example:', len(examples)) #28483
+    ## structure of examples
+        # print(examples[i].text_a) 
+        # print(examples[i].text_b)
+        # print(examples[i].label)
+    
+    # turn examples into BERT Tokenized Ids (features)
+
+    features = convert_examples_to_features(examples, label_list, max_seq_length, tokenizer_mlm, "classification",
+                                            cls_token_at_end=bool(model_type in ['xlnet']),
+                                            # xlnet has a cls token at the end
+                                            cls_token=tokenizer_mlm.cls_token,
+                                            sep_token=tokenizer_mlm.sep_token,
+                                            cls_token_segment_id=2 if model_type in ['xlnet'] else 1,
+                                            pad_on_left=bool(model_type in ['xlnet']),
+                                            # pad on the left for xlnet
+                                            pad_token_segment_id=4 if model_type in ['xlnet'] else 0)
+
+    # To-Do: 可以将结果保存为cache，这样就不需要每次都重新加载了
+
+    ###--------- Convert to Tensors and build dataset --------------------------
+    
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)     #read input_ids of each data point; turn into tensor; store them in a list
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_id for f in features], dtype=torch.long)
+    
+    #print('these are sizes:', all_input_ids.size(), all_input_mask.size(), all_segment_ids.size(), all_label_ids.size())  # ---> [num_data_items, max_length]*3, [num_data_items] --> [28483,200]*3, [28483]
+    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+
+    eval_sampler = SequentialSampler(dataset)
+    eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=16)
+
+
+
+
 
 
 
