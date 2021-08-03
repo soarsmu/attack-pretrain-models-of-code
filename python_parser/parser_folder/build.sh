82f28adebf9d1c831638cbfbe4db44230ea191e8
dennis9707 <he0707@hotmail.com>
2021-08-03 13:45:45 +0800

    updated .so file and build functions

--- python_parser/parser_folder/build.sh
+++ python_parser/parser_folder/build.sh
@@ -1,8 +1,4 @@
-git clone https://github.com/tree-sitter/tree-sitter-go
-git clone https://github.com/tree-sitter/tree-sitter-javascript
 git clone https://github.com/tree-sitter/tree-sitter-python
-git clone https://github.com/tree-sitter/tree-sitter-ruby
-git clone https://github.com/tree-sitter/tree-sitter-php
 git clone https://github.com/tree-sitter/tree-sitter-java
-git clone https://github.com/tree-sitter/tree-sitter-c-sharp
+git clone https://github.com/tree-sitter/tree-sitter-c
 python build.py
