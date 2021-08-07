ad16cb6e21b3de20620b7bdd5b5088e114378c64
dennis9707 <he0707@hotmail.com>
2021-08-08 00:25:53 +0800

    Reformat code to coding styles

--- python_parser/run_parser.py
+++ python_parser/run_parser.py
@@ -1,9 +1,9 @@
 import argparse
 from parser_folder.DFG import DFG_python, DFG_java, DFG_c
 from parser_folder import (remove_comments_and_docstrings,
-                    tree_to_token_index,
-                    index_to_code_token,
-                    tree_to_variable_index)
+                           tree_to_token_index,
+                           index_to_code_token,
+                           tree_to_variable_index)
 from tree_sitter import Language, Parser
 
 path = 'parser_folder/my-languages.so'
@@ -39,67 +39,69 @@ java_code = """
         }
     }  
 """
-dfg_function={
-    'python':DFG_python,
-    'java':DFG_java,
-    'c':DFG_c,
+dfg_function = {
+    'python': DFG_python,
+    'java': DFG_java,
+    'c': DFG_c,
 }
 
-#load parsers
-parsers={}
+# load parsers
+parsers = {}
 for lang in dfg_function:
     LANGUAGE = Language('parser_folder/my-languages.so', lang)
     parser = Parser()
     parser.set_language(LANGUAGE)
-    parser = [parser,dfg_function[lang]]
-    parsers[lang]= parser
+    parser = [parser, dfg_function[lang]]
+    parsers[lang] = parser
 
 codes = {}
-codes ={
-    'python':python_code,
-    'java':java_code,
-    'c':c_code,
+codes = {
+    'python': python_code,
+    'java': java_code,
+    'c': c_code,
 }
 
+
 def extract_dataflow(code, lang):
-    #remove comments
+    # remove comments
     try:
-        code=remove_comments_and_docstrings(code, lang)
+        code = remove_comments_and_docstrings(code, lang)
     except:
         pass
-        #obtain dataflow
-    parser=parsers[lang]
-    tree = parser[0].parse(bytes(code,'utf8'))
+        # obtain dataflow
+    parser = parsers[lang]
+    tree = parser[0].parse(bytes(code, 'utf8'))
     root_node = tree.root_node
-    tokens_index=tree_to_token_index(root_node)
-    code=code.split('\n')
-    code_tokens=[index_to_code_token(x,code) for x in tokens_index]
-    index_to_code={}
-    for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
-        index_to_code[index]=(idx,code)
+    tokens_index = tree_to_token_index(root_node)
+    code = code.split('\n')
+    code_tokens = [index_to_code_token(x, code) for x in tokens_index]
+    index_to_code = {}
+    for idx, (index, code) in enumerate(zip(tokens_index, code_tokens)):
+        index_to_code[index] = (idx, code)
 
-    index_table={}
-    for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
-        index_table[idx]=index
+    index_table = {}
+    for idx, (index, code) in enumerate(zip(tokens_index, code_tokens)):
+        index_table[idx] = index
     try:
         # call dfg_python here
-        DFG,_=parser[1](root_node,index_to_code,{})
+        DFG, _ = parser[1](root_node, index_to_code, {})
     except:
-        DFG=[]
-    DFG=sorted(DFG,key=lambda x:x[1])
+        DFG = []
+    DFG = sorted(DFG, key=lambda x: x[1])
     return DFG, index_table
+
 
 def parse_string(input):
     if (input.startswith("\"\"\"") and input.endswith("\"\"\"")) or \
-        (input.startswith("\'\'\'") and input.endswith("\'\'\'")) or \
-        (input.startswith("\'") and input.endswith("\'")) or \
-        (input.startswith("\"") and input.endswith("\"")):
+            (input.startswith("\'\'\'") and input.endswith("\'\'\'")) or \
+            (input.startswith("\'") and input.endswith("\'")) or \
+            (input.startswith("\"") and input.endswith("\"")):
         return True
     return False
 
 
 def get_identifiers(code, lang):
-    parser=parsers[lang]
+    parser = parsers[lang]
     dfg, index_table = extract_dataflow(code, lang)
     print("dfg")
     for d in dfg:
@@ -107,7 +109,7 @@ def get_identifiers(code, lang):
     ret = []
     ret_set = set()
     for d in dfg:
-        if d[0].replace('.','',1).isdigit() or parse_string(d[0]):
+        if d[0].replace('.', '', 1).isdigit() or parse_string(d[0]):
             # skip if it is a number
             continue
         if len(d[-1]) == 0 or d[2] == 'computedFrom' or (d[2] == 'comesFrom' and d[0] != d[-2][0]):
@@ -117,7 +119,7 @@ def get_identifiers(code, lang):
                 # print(entry)
                 ret.append(entry)
                 ret_set.add(tuple([d[1]]))
-                sorted(ret, key=lambda x:x[1][0])
+                sorted(ret, key=lambda x: x[1][0])
         else:
             for r in ret:
                 if d[-1][0] in r[1]:
@@ -131,9 +133,9 @@ def main():
     parser.add_argument("--lang", default=None, type=str,
                         help="language.")
     args = parser.parse_args()
-    parser=parsers[args.lang]
+    parser = parsers[args.lang]
     code = codes[args.lang]
-    #extract data flow
+    # extract data flow
     data = get_identifiers(code, args.lang)
     print("final ret")
     for identifier in data:
@@ -142,4 +144,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
