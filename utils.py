fb81541819a1db48933a76bfea304caffb6f966c
Yang Zhou <zhou.yang.19@ucl.ac.uk>
2021-08-10 17:30:08 +0800

    refactor(): move functions to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+
+
 python_keywords = ['import', '', '[', ']', ':', ',', '.', '(', ')', '{', '}', 'not', 'is', '=', "+=", '-=', "<", ">", '+', '-', '*', '/', 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 
 special_char = ['[', ']', ':', ',', '.', '(', ')', '{', '}', 'not', 'is', '=', "+=", '-=', "<", ">", '+', '-', '*', '/', '|']
@@ -32,3 +36,131 @@ def is_valid_substitue(substitute: str, tgt_word: str) -> bool:
         is_valid = False
 
     return is_valid
+
+
+def _tokenize(seq, tokenizer):
+    seq = seq.replace('\n', '').lower()
+    words = seq.split(' ')
+
+    sub_words = []
+    keys = []
+    index = 0
+    for word in words:
+        # 并非直接tokenize这句话，而是tokenize了每个splited words.
+        sub = tokenizer.tokenize(word)
+        sub_words += sub
+        keys.append([index, index + len(sub)])
+        # 将subwords对齐
+        index += len(sub)
+
+    return words, sub_words, keys
+
+
+def get_identifier_posistions_from_code(words_list: list, variable_names: list) -> dict:
+    '''
+    给定一串代码，以及variable的变量名，如: a
+    返回这串代码中这些变量名对应的位置.
+    '''
+    positions = {}
+    for name in variable_names:
+        for index, token in enumerate(words_list):
+            if name == token:
+                try:
+                    positions[name].append(index)
+                except:
+                    positions[name] = [index]
+
+    return positions
+
+def get_bpe_substitues(substitutes, tokenizer, mlm_model):
+    '''
+    得到substitues
+    '''
+    # substitutes L, k
+
+    substitutes = substitutes[0:12, 0:4] # maximum BPE candidates
+
+    # find all possible candidates 
+
+    all_substitutes = []
+    for i in range(substitutes.size(0)):
+        if len(all_substitutes) == 0:
+            lev_i = substitutes[i]
+            all_substitutes = [[int(c)] for c in lev_i]
+        else:
+            lev_i = []
+            for all_sub in all_substitutes[:24]: # 去掉不用的计算.
+                for j in substitutes[i]:
+                    lev_i.append(all_sub + [int(j)])
+            all_substitutes = lev_i
+    # all substitutes  list of list of token-id (all candidates)
+    c_loss = nn.CrossEntropyLoss(reduction='none')
+    word_list = []
+    # all_substitutes = all_substitutes[:24]
+    all_substitutes = torch.tensor(all_substitutes) # [ N, L ]
+    all_substitutes = all_substitutes[:24].to('cuda')
+    # 不是，这个总共不会超过24... 那之前生成那么多也没用....
+    # print(substitutes.size(), all_substitutes.size())
+    N, L = all_substitutes.size()
+    word_predictions = mlm_model(all_substitutes)[0] # N L vocab-size
+    ppl = c_loss(word_predictions.view(N*L, -1), all_substitutes.view(-1)) # [ N*L ] 
+    ppl = torch.exp(torch.mean(ppl.view(N, L), dim=-1)) # N  
+    _, word_list = torch.sort(ppl)
+    word_list = [all_substitutes[i] for i in word_list]
+    final_words = []
+    for word in word_list:
+        tokens = [tokenizer._convert_id_to_token(int(i)) for i in word]
+        text = tokenizer.convert_tokens_to_string(tokens)
+        final_words.append(text)
+    return final_words
+
+
+
+
+def get_substitues(substitutes, tokenizer, mlm_model, use_bpe, substitutes_score=None, threshold=3.0):
+    '''
+    将生成的substitued subwords转化为words
+    '''
+    # substitues L,k
+    # from this matrix to recover a word
+    words = []
+    sub_len, k = substitutes.size()  # sub-len, k
+
+    if sub_len == 0:
+        # 比如空格对应的subwords就是[a,a]，长度为0
+        return words
+        
+    elif sub_len == 1:
+        # subwords就是本身
+        for (i,j) in zip(substitutes[0], substitutes_score[0]):
+            if threshold != 0 and j < threshold:
+                break
+            words.append(tokenizer._convert_id_to_token(int(i)))
+            # 将id转为token.
+    else:
+        # word被分解成了多个subwords
+        if use_bpe == 1:
+            words = get_bpe_substitues(substitutes, tokenizer, mlm_model)
+        else:
+            return words
+    return words
+
+
+def get_masked_code_by_position(tokens: list, positions: dict):
+    '''
+    给定一段文本，以及需要被mask的位置,返回一组masked后的text
+    Example:
+        tokens: [a,b,c]
+        positions: [0,2]
+        Return:
+            [<mask>, b, c]
+            [a, b, <mask>]
+    '''
+    masked_token_list = []
+    replace_token_positions = []
+    for variable_name in positions.keys():
+        for pos in positions[variable_name]:
+            masked_token_list.append(tokens[0:pos] + ['<unk>'] + tokens[pos + 1:])
+            replace_token_positions.append(pos)
+    
+    return masked_token_list, replace_token_positions
