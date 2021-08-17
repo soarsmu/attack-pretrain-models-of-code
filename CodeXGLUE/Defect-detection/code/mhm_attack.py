be31b8f64e2468c0b196bef1af4dc88989ceeec1
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-08-17 17:45:57 +0800

    feat(mhm): load pretrained models

--- CodeXGLUE/Defect-detection/code/mhm_attack.py
+++ CodeXGLUE/Defect-detection/code/mhm_attack.py
@@ -3,6 +3,43 @@ import random
 import torch
 import numpy
 import copy
+import enum
+import sys
+import os
+
+sys.path.append('../../../')
+sys.path.append('../../../python_parser')
+retval = os.getcwd()
+
+import csv
+import copy
+import json
+import logging
+import argparse
+import warnings
+import torch
+import numpy as np
+import random
+from model import Model
+from run import set_seed
+from run import TextDataset
+from run import InputFeatures
+from utils import select_parents, crossover, map_chromesome, mutate, python_keywords, is_valid_substitue, _tokenize
+from utils import get_identifier_posistions_from_code
+from utils import get_masked_code_by_position, get_substitues
+from run_parser import get_identifiers
+
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import SequentialSampler, DataLoader
+from transformers import RobertaForMaskedLM
+from transformers import (RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning) # Only report warning\
+
+MODEL_CLASSES = {
+    'roberta': (RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
+}
 
 from utils import getUID, isUID, getTensor
 
@@ -140,11 +177,167 @@ if __name__ == "__main__":
     import time
     import os
     
-    import tree as Tree
-    from dataset import Dataset, POJ104_SEQ
-    from lstm_classifier import LSTMEncoder, LSTMClassifier
+    # import tree as Tree
+    # from dataset import Dataset, POJ104_SEQ
+    # from lstm_classifier import LSTMEncoder, LSTMClassifier
     
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--train_data_file", default=None, type=str, required=True,
+                        help="The input training data file (a text file).")
+    parser.add_argument("--output_dir", default=None, type=str, required=True,
+                        help="The output directory where the model predictions and checkpoints will be written.")
+
+    ## Other parameters
+    parser.add_argument("--eval_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+    parser.add_argument("--test_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+                    
+    parser.add_argument("--model_type", default="bert", type=str,
+                        help="The model architecture to be fine-tuned.")
+    parser.add_argument("--model_name_or_path", default=None, type=str,
+                        help="The model checkpoint for weights initialization.")
+
+    parser.add_argument("--base_model", default=None, type=str,
+                        help="Base Model")
+    parser.add_argument("--csv_store_path", default=None, type=str,
+                        help="Base Model")
+
+    parser.add_argument("--mlm", action='store_true',
+                        help="Train with masked-language modeling loss instead of language modeling.")
+    parser.add_argument("--mlm_probability", type=float, default=0.15,
+                        help="Ratio of tokens to mask for masked language modeling loss")
+
+    parser.add_argument("--config_name", default="", type=str,
+                        help="Optional pretrained config name or path if not the same as model_name_or_path")
+    parser.add_argument("--tokenizer_name", default="", type=str,
+                        help="Optional pretrained tokenizer name or path if not the same as model_name_or_path")
+    parser.add_argument("--cache_dir", default="", type=str,
+                        help="Optional directory to store the pre-trained models downloaded from s3 (instread of the default one)")
+    parser.add_argument("--block_size", default=-1, type=int,
+                        help="Optional input sequence length after tokenization."
+                             "The training dataset will be truncated in block of this size for training."
+                             "Default to the model max input length for single sentence inputs (take into account special tokens).")
+    parser.add_argument("--do_train", action='store_true',
+                        help="Whether to run training.")
+    parser.add_argument("--do_eval", action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_test", action='store_true',
+                        help="Whether to run eval on the dev set.")    
+    parser.add_argument("--evaluate_during_training", action='store_true',
+                        help="Run evaluation during training at each logging step.")
+    parser.add_argument("--do_lower_case", action='store_true',
+                        help="Set this flag if you are using an uncased model.")
+
+    parser.add_argument("--train_batch_size", default=4, type=int,
+                        help="Batch size per GPU/CPU for training.")
+    parser.add_argument("--eval_batch_size", default=4, type=int,
+                        help="Batch size per GPU/CPU for evaluation.")
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
+                        help="Number of updates steps to accumulate before performing a backward/update pass.")
+    parser.add_argument("--learning_rate", default=5e-5, type=float,
+                        help="The initial learning rate for Adam.")
+    parser.add_argument("--weight_decay", default=0.0, type=float,
+                        help="Weight deay if we apply some.")
+    parser.add_argument("--adam_epsilon", default=1e-8, type=float,
+                        help="Epsilon for Adam optimizer.")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float,
+                        help="Max gradient norm.")
+    parser.add_argument("--num_train_epochs", default=1.0, type=float,
+                        help="Total number of training epochs to perform.")
+    parser.add_argument("--max_steps", default=-1, type=int,
+                        help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
+    parser.add_argument("--warmup_steps", default=0, type=int,
+                        help="Linear warmup over warmup_steps.")
+
+    parser.add_argument('--logging_steps', type=int, default=50,
+                        help="Log every X updates steps.")
+    parser.add_argument('--save_steps', type=int, default=50,
+                        help="Save checkpoint every X updates steps.")
+    parser.add_argument('--save_total_limit', type=int, default=None,
+                        help='Limit the total amount of checkpoints, delete the older checkpoints in the output_dir, does not delete by default')
+    parser.add_argument("--eval_all_checkpoints", action='store_true',
+                        help="Evaluate all checkpoints starting with the same prefix as model_name_or_path ending and ending with step number")
+    parser.add_argument("--no_cuda", action='store_true',
+                        help="Avoid using CUDA when available")
+    parser.add_argument('--overwrite_output_dir', action='store_true',
+                        help="Overwrite the content of the output directory")
+    parser.add_argument('--overwrite_cache', action='store_true',
+                        help="Overwrite the cached training and evaluation sets")
+    parser.add_argument('--seed', type=int, default=42,
+                        help="random seed for initialization")
+    parser.add_argument('--epoch', type=int, default=42,
+                        help="random seed for initialization")
+    parser.add_argument('--fp16', action='store_true',
+                        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
+    parser.add_argument('--fp16_opt_level', type=str, default='O1',
+                        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+                             "See details at https://nvidia.github.io/apex/amp.html")
+    parser.add_argument("--local_rank", type=int, default=-1,
+                        help="For distributed training: local_rank")
+    parser.add_argument('--server_ip', type=str, default='', help="For distant debugging.")
+    parser.add_argument('--server_port', type=str, default='', help="For distant debugging.")
+
+
+    args = parser.parse_args()
+
+
+    args.device = torch.device("cuda")
+    # Set seed
+    set_seed(args.seed)
+
+
+    args.start_epoch = 0
+    args.start_step = 0
+
+    ## Load Target Model
+    checkpoint_last = os.path.join(args.output_dir, 'checkpoint-last') # 读取model的路径
+    if os.path.exists(checkpoint_last) and os.listdir(checkpoint_last):
+        # 如果路径存在且有内容，则从checkpoint load模型
+        args.model_name_or_path = os.path.join(checkpoint_last, 'pytorch_model.bin')
+        args.config_name = os.path.join(checkpoint_last, 'config.json')
+        idx_file = os.path.join(checkpoint_last, 'idx_file.txt')
+        with open(idx_file, encoding='utf-8') as idxf:
+            args.start_epoch = int(idxf.readlines()[0].strip()) + 1
+
+        step_file = os.path.join(checkpoint_last, 'step_file.txt')
+        if os.path.exists(step_file):
+            with open(step_file, encoding='utf-8') as stepf:
+                args.start_step = int(stepf.readlines()[0].strip())
+        logger.info("reload model from {}, resume from {} epoch".format(checkpoint_last, args.start_epoch))
+
+
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+    config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path,
+                                          cache_dir=args.cache_dir if args.cache_dir else None)
+    config.num_labels=1 # 只有一个label?
+    tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name,
+                                                do_lower_case=args.do_lower_case,
+                                                cache_dir=args.cache_dir if args.cache_dir else None)
+    if args.block_size <= 0:
+        args.block_size = tokenizer.max_len_single_sentence  # Our input block size will be the max possible for the model
+    args.block_size = min(args.block_size, tokenizer.max_len_single_sentence)
+    if args.model_name_or_path:
+        model = model_class.from_pretrained(args.model_name_or_path,
+                                            from_tf=bool('.ckpt' in args.model_name_or_path),
+                                            config=config,
+                                            cache_dir=args.cache_dir if args.cache_dir else None)    
+    else:
+        model = model_class(config)
+
+    model = Model(model,config,tokenizer,args)
+
+
+    checkpoint_prefix = 'checkpoint-best-acc/model.bin'
+    output_dir = os.path.join(args.output_dir, '{}'.format(checkpoint_prefix))  
+    model.load_state_dict(torch.load(output_dir))      
+    model.to(args.device)
+    # 会是因为模型不同吗？我看evaluate的时候模型是重新导入的.
+
+
+    exit()
     
     model_path = "../LSTMClassifier/saved_models/3.pt" # target model
     data_path = "../../preprocess/dataset/oj.pkl" # all the dataset (train + test)
@@ -158,6 +351,9 @@ if __name__ == "__main__":
     n_layers = 2
     num_classes = 104
     max_len = 500
+
+    # Load Models
+
     
     enc = LSTMEncoder(embedding_size, hidden_size, n_layers)
     classifier = LSTMClassifier(vocab_size, embedding_size, enc,
