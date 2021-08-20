8d1b16e85124f3c10bf15067fc8fdf0b655f0583
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-08-20 14:31:51 +0800

    chore(): remove package

--- python_parser/parser_folder/__init__.py
+++ python_parser/parser_folder/__init__.py
@@ -2,4 +2,3 @@ from .utils import (remove_comments_and_docstrings,
                    tree_to_token_index,
                    index_to_code_token,
                    tree_to_variable_index)
-from .DFG import DFG_python,DFG_java
