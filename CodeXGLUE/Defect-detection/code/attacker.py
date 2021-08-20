664e84953a53ba627dec507bb235942939be8b14
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-08-20 16:43:26 +0800

    feat(mhm): record number of changes

--- CodeXGLUE/Defect-detection/code/attacker.py
+++ CodeXGLUE/Defect-detection/code/attacker.py
@@ -161,7 +161,6 @@ class Attacker():
         names_positions_dict = get_identifier_posistions_from_code(words, variable_names)
 
 
-        final_words = copy.deepcopy(words)
         
         nb_changed_var = 0 # 表示被修改的variable数量
         nb_changed_pos = 0
@@ -294,7 +293,12 @@ class Attacker():
             for index, logits in enumerate(mutate_logits):
                 if mutate_preds[index] != orig_label:
                     adv_code = " ".join(map_chromesome(_temp_mutants[index], words, names_positions_dict))
-                    return code, prog_length, adv_code, true_label, orig_label, mutate_preds[index], 1, variable_names, None, None, None, child_1
+                    for old_word in variable_names.keys():
+                        if old_word == variable_names[old_word]:
+                            nb_changed_var += 1
+                            nb_changed_pos += len(names_positions_dict[old_word])
+
+                    return code, prog_length, adv_code, true_label, orig_label, mutate_preds[index], 1, variable_names, nb_changed_var, nb_changed_pos, child_1
                 _tmp_fitness = max(orig_prob) - logits[orig_label]
                 mutate_fitness_values.append(_tmp_fitness)
             
@@ -307,7 +311,7 @@ class Attacker():
                     population[min_index] = _temp_mutants[index]
                     fitness_values[min_index] = fitness_value
 
-        return code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, None, None, None, None
+        return code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, None, nb_changed_var, nb_changed_pos, None
         
 
 
