9ce4115383826f01b6f725affd0a8bf3e958aae7
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-08-20 14:32:14 +0800

    feat(MHM): MHM_Attacker class

--- CodeXGLUE/Defect-detection/code/attacker.py
+++ CodeXGLUE/Defect-detection/code/attacker.py
@@ -18,6 +18,7 @@ from run import TextDataset, InputFeatures
 from utils import select_parents, crossover, map_chromesome, mutate, is_valid_variable_name, _tokenize, get_identifier_posistions_from_code, get_masked_code_by_position, get_substitues, is_valid_substitue, set_seed
 
 from utils import CodeDataset
+from utils import getUID, isUID, getTensor, build_vocab
 from run_parser import get_identifiers
 from transformers import (RobertaForMaskedLM, RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
 
@@ -535,3 +536,176 @@ class Attacker():
 
 
 
+class MHM_Attacker():
+    def __init__(self, args, model_tgt, model_mlm, tokenizer_mlm, _token2idx, _idx2token) -> None:
+        self.classifier = model_tgt
+        self.model_mlm = model_mlm
+        self.token2idx = _token2idx
+        self.idx2token = _idx2token
+        self.args = args
+        self.tokenizer_mlm = tokenizer_mlm
+    
+    def mcmc(self, tokenizer, code=None, _label=None, _n_candi=30,
+             _max_iter=100, _prob_threshold=0.95):
+        identifiers, code_tokens = get_identifiers(code, 'c')
+        processed_code = " ".join(code_tokens)
+
+        words, sub_words, keys = _tokenize(processed_code, tokenizer)
+        raw_tokens = copy.deepcopy(words)
+        variable_names = []
+        for name in identifiers:
+            if ' ' in name[0].strip():
+                continue
+            variable_names.append(name[0])
+        uid = get_identifier_posistions_from_code(words, variable_names)
+
+        if len(uid) <= 0: # 是有可能存在找不到变量名的情况的.
+            return {'succ': None, 'tokens': None, 'raw_tokens': None}
+
+        # 还需要得到substitues
+
+        sub_words = [tokenizer.cls_token] + sub_words[:self.args.block_size - 2] + [tokenizer.sep_token]
+        # 如果长度超了，就截断；这里的block_size是CodeBERT能接受的输入长度
+        input_ids_ = torch.tensor([tokenizer.convert_tokens_to_ids(sub_words)])
+        word_predictions = self.model_mlm(input_ids_.to('cuda'))[0].squeeze()  # seq-len(sub) vocab
+        word_pred_scores_all, word_predictions = torch.topk(word_predictions, 30, -1)  # seq-len k
+        # 得到前k个结果.
+
+        word_predictions = word_predictions[1:len(sub_words) + 1, :]
+        word_pred_scores_all = word_pred_scores_all[1:len(sub_words) + 1, :]
+        # 只取subwords的部分，忽略首尾的预测结果.
+
+
+        variable_substitue_dict = {}
+        for tgt_word in uid.keys():
+            if not is_valid_variable_name(tgt_word, 'c'):
+                # 如果不是变量名
+                continue   
+            tgt_positions = uid[tgt_word] # 在words中对应的位置
+
+            ## 得到(所有位置的)substitues
+            all_substitues = []
+            for one_pos in tgt_positions:
+                ## 一个变量名会出现很多次
+                substitutes = word_predictions[keys[one_pos][0]:keys[one_pos][1]]  # L, k
+                word_pred_scores = word_pred_scores_all[keys[one_pos][0]:keys[one_pos][1]]
+
+                substitutes = get_substitues(substitutes, 
+                                            self.tokenizer_mlm, 
+                                            self.classifier, 
+                                            1, 
+                                            word_pred_scores, 
+                                            0)
+                all_substitues += substitutes
+            all_substitues = set(all_substitues)
+
+            for tmp_substitue in all_substitues:
+                if not is_valid_substitue(tmp_substitue, tgt_word, 'c'):
+                    continue
+                try:
+                    variable_substitue_dict[tgt_word].append(tmp_substitue)
+                except:
+                    variable_substitue_dict[tgt_word] = [tmp_substitue]
+        
+
+        for iteration in range(1, 1+_max_iter):
+            # 这个函数需要tokens
+            res = self.__replaceUID(_tokens=words, _label=_label, _uid=uid,
+                                    substitute_dict=variable_substitue_dict,
+                                    _n_candi=_n_candi,
+                                    _prob_threshold=_prob_threshold)
+            self.__printRes(_iter=iteration, _res=res, _prefix="  >> ")
+            if res['status'].lower() in ['s', 'a']:
+                tokens = res['tokens']
+                uid[res['new_uid']] = uid.pop(res['old_uid']) # 替换key，但保留value.
+                variable_substitue_dict[res['new_uid']] = variable_substitue_dict.pop(res['old_uid'])
+                for i in range(len(raw_tokens)):
+                    if raw_tokens[i] == res['old_uid']:
+                        raw_tokens[i] = res['new_uid']
+                if res['status'].lower() == 's':
+                    return {'succ': True, 'tokens': tokens,
+                            'raw_tokens': raw_tokens}
+
+        return {'succ': False, 'tokens': None, 'raw_tokens': None}
+        
+    def __replaceUID(self, _tokens=[], _label=None, _uid={}, substitute_dict={},
+                     _n_candi=30, _prob_threshold=0.95, _candi_mode="random"):
+        
+        assert _candi_mode.lower() in ["random", "nearby"]
+        
+        selected_uid = random.sample(substitute_dict.keys(), 1)[0] # 选择需要被替换的变量名
+        if _candi_mode == "random":
+            # First, generate candidate set.
+            # The transition probabilities of all candidate are the same.
+            candi_token = [selected_uid]
+            candi_tokens = [copy.deepcopy(_tokens)]
+            candi_labels = [_label]
+            for c in random.sample(substitute_dict[selected_uid], min(_n_candi, len(substitute_dict[selected_uid]))): # 选出_n_candi数量的候选.
+                if isUID(c): # 判断是否是变量名.
+                    candi_token.append(c)
+                    candi_tokens.append(copy.deepcopy(_tokens))
+                    candi_labels.append(_label)
+                    for i in _uid[selected_uid]: # 依次进行替换.
+                        if i >= len(candi_tokens[-1]):
+                            break
+                        candi_tokens[-1][i] = c # 替换为新的candidate.
+
+            new_example = []
+            for tmp_tokens in candi_tokens:
+                tmp_code = " ".join(tmp_tokens)
+                new_feature = convert_code_to_features(tmp_code, self.tokenizer_mlm, _label, self.args)
+                new_example.append(new_feature)
+            new_dataset = CodeDataset(new_example)
+            prob, pred = self.classifier.get_results(new_dataset, self.args.eval_batch_size)
+
+            for i in range(len(candi_token)):   # Find a valid example
+                if pred[i] != _label: # 如果有样本攻击成功
+                    return {"status": "s", "alpha": 1, "tokens": candi_tokens[i],
+                            "old_uid": selected_uid, "new_uid": candi_token[i],
+                            "old_prob": prob[0], "new_prob": prob[i],
+                            "old_pred": pred[0], "new_pred": pred[i]}
+
+            candi_idx = 0
+            min_prob = 1.0
+
+            for idx, a_prob in enumerate(prob[1:]):
+                if a_prob[_label] < min_prob:
+                    candi_idx = idx + 1
+                    min_prob = a_prob[_label]
+
+            # 找到Ground_truth对应的probability最小的那个mutant
+            # At last, compute acceptance rate.
+            alpha = (1-prob[candi_idx][_label]+1e-10) / (1-prob[0][_label]+1e-10)
+            # 计算这个id对应的alpha值.
+            if random.uniform(0, 1) > alpha or alpha < _prob_threshold:
+                return {"status": "r", "alpha": alpha, "tokens": candi_tokens[i],
+                        "old_uid": selected_uid, "new_uid": candi_token[i],
+                        "old_prob": prob[0], "new_prob": prob[i],
+                        "old_pred": pred[0], "new_pred": pred[i]}
+            else:
+                return {"status": "a", "alpha": alpha, "tokens": candi_tokens[i],
+                        "old_uid": selected_uid, "new_uid": candi_token[i],
+                        "old_prob": prob[0], "new_prob": prob[i],
+                        "old_pred": pred[0], "new_pred": pred[i]}
+        else:
+            pass
+
+    def __printRes(self, _iter=None, _res=None, _prefix="  => "):
+        if _res['status'].lower() == 's':   # Accepted & successful
+            print("%s iter %d, SUCC! %s => %s (%d => %d, %.5f => %.5f) a=%.3f" % \
+                  (_prefix, _iter, _res['old_uid'], _res['new_uid'],
+                   _res['old_pred'], _res['new_pred'],
+                   _res['old_prob'][_res['old_pred']],
+                   _res['new_prob'][_res['old_pred']], _res['alpha']), flush=True)
+        elif _res['status'].lower() == 'r': # Rejected
+            print("%s iter %d, REJ. %s => %s (%d => %d, %.5f => %.5f) a=%.3f" % \
+                  (_prefix, _iter, _res['old_uid'], _res['new_uid'],
+                   _res['old_pred'], _res['new_pred'],
+                   _res['old_prob'][_res['old_pred']],
+                   _res['new_prob'][_res['old_pred']], _res['alpha']), flush=True)
+        elif _res['status'].lower() == 'a': # Accepted
+            print("%s iter %d, ACC! %s => %s (%d => %d, %.5f => %.5f) a=%.3f" % \
+                  (_prefix, _iter, _res['old_uid'], _res['new_uid'],
+                   _res['old_pred'], _res['new_pred'],
+                   _res['old_prob'][_res['old_pred']],
+                   _res['new_prob'][_res['old_pred']], _res['alpha']), flush=True)
