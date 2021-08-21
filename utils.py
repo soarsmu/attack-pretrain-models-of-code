7a85e0c69240c2e2fcda29dde23447b70f5f1898
jiekeshi <jiekeshi125@gmail.com>
2021-08-21 13:48:15 +0800

    feat(graphcodebert): new dataset class

--- utils.py
+++ utils.py
@@ -545,6 +545,43 @@ class CodeDataset(Dataset):
     def __getitem__(self, i):       
         return torch.tensor(self.examples[i].input_ids),torch.tensor(self.examples[i].label)
 
+class GraphCodeDataset(Dataset):
+    def __init__(self, examples, args):
+        self.examples = examples
+        self.args=args
+    
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        #calculate graph-guided masked function
+        attn_mask=np.zeros((self.args.code_length+self.args.data_flow_length,
+                            self.args.code_length+self.args.data_flow_length),dtype=np.bool)
+        #calculate begin index of node and max length of input
+        
+        node_index=sum([i>1 for i in self.examples[item].position_idx])
+        max_length=sum([i!=1 for i in self.examples[item].position_idx])
+        #sequence can attend to sequence
+        attn_mask[:node_index,:node_index]=True
+        #special tokens attend to all tokens
+        for idx,i in enumerate(self.examples[item].input_ids):
+            if i in [0,2]:
+                attn_mask[idx,:max_length]=True
+        #nodes attend to code tokens that are identified from
+        for idx,(a,b) in enumerate(self.examples[item].dfg_to_code):
+            if a<node_index and b<node_index:
+                attn_mask[idx+node_index,a:b]=True
+                attn_mask[a:b,idx+node_index]=True
+        #nodes attend to adjacent nodes 
+        for idx,nodes in enumerate(self.examples[item].dfg_to_dfg):
+            for a in nodes:
+                if a+node_index<len(self.examples[item].position_idx):
+                    attn_mask[idx+node_index,a+node_index]=True
+              
+        return (torch.tensor(self.examples[item].input_ids),
+              torch.tensor(attn_mask),
+              torch.tensor(self.examples[item].position_idx),
+              torch.tensor(self.examples[item].label))
 
 def set_seed(seed=42):
     random.seed(seed)
