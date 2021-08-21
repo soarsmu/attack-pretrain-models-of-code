c71e598c8d1efdd5452ed83377d76d634a1249ba
jiekeshi <jiekeshi125@gmail.com>
2021-08-21 13:49:13 +0800

    feat(): gi_attack for graphcodebert

---
+++ GraphCodeBERT/Defect-detection/code/gi_attack.py
@@ -0,0 +1,207 @@
+# coding=utf-8
+# @Time    : 2020/8/13
+# @Author  : Zhou Yang
+# @Email   : zyang@smu.edu.sg
+# @File    : gi_attack.py
+'''For attacking CodeBERT models'''
+import sys
+import os
+
+sys.path.append('../../../')
+sys.path.append('../../../python_parser')
+
+import json
+import logging
+import argparse
+import warnings
+import torch
+from model import Model
+from run import TextDataset
+from utils import set_seed
+
+from utils import Recorder
+from attacker import Attacker
+from transformers import (RobertaForMaskedLM, RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.simplefilter(action='ignore', category=FutureWarning) # Only report warning
+
+MODEL_CLASSES = {
+    'roberta': (RobertaConfig, RobertaForSequenceClassification, RobertaTokenizer)
+}
+
+logger = logging.getLogger(__name__)
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--train_data_file", default=None, type=str, required=True,
+                        help="The input training data file (a text file).")
+    parser.add_argument("--output_dir", default=None, type=str, required=True,
+                        help="The output directory where the model predictions and checkpoints will be written.")
+
+    ## Other parameters
+    parser.add_argument("--eval_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+    parser.add_argument("--test_data_file", default=None, type=str,
+                        help="An optional input evaluation data file to evaluate the perplexity on (a text file).")
+                    
+    parser.add_argument("--model_type", default="bert", type=str,
+                        help="The model architecture to be fine-tuned.")
+    parser.add_argument("--model_name_or_path", default=None, type=str,
+                        help="The model checkpoint for weights initialization.")
+
+    parser.add_argument("--base_model", default=None, type=str,
+                        help="Base Model")
+    parser.add_argument("--csv_store_path", default=None, type=str,
+                        help="Base Model")
+
+    parser.add_argument("--mlm", action='store_true',
+                        help="Train with masked-language modeling loss instead of language modeling.")
+    parser.add_argument("--mlm_probability", type=float, default=0.15,
+                        help="Ratio of tokens to mask for masked language modeling loss")
+
+    parser.add_argument("--config_name", default="", type=str,
+                        help="Optional pretrained config name or path if not the same as model_name_or_path")
+    parser.add_argument("--tokenizer_name", default="", type=str,
+                        help="Optional pretrained tokenizer name or path if not the same as model_name_or_path")
+    parser.add_argument("--data_flow_length", default=64, type=int,
+                        help="Optional Data Flow input sequence length after tokenization.") 
+    parser.add_argument("--code_length", default=256, type=int,
+                        help="Optional Code input sequence length after tokenization.") 
+    parser.add_argument("--do_train", action='store_true',
+                        help="Whether to run training.")
+    parser.add_argument("--use_ga", action='store_true',
+                        help="Whether to GA-Attack.")
+    parser.add_argument("--do_eval", action='store_true',
+                        help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_test", action='store_true',
+                        help="Whether to run eval on the dev set.")    
+    parser.add_argument("--eval_batch_size", default=4, type=int,
+                        help="Batch size per GPU/CPU for evaluation.")
+    parser.add_argument('--seed', type=int, default=42,
+                        help="random seed for initialization")
+    parser.add_argument("--cache_dir", default="", type=str,
+                        help="Optional directory to store the pre-trained models downloaded from s3 (instread of the default one)")
+
+
+
+    args = parser.parse_args()
+
+
+    args.device = torch.device("cuda")
+    # Set seed
+    set_seed(args.seed)
+
+
+    args.start_epoch = 0
+    args.start_step = 0
+
+
+    ## Load Target Model
+    checkpoint_last = os.path.join(args.output_dir, 'checkpoint-last') # 读取model的路径
+    if os.path.exists(checkpoint_last) and os.listdir(checkpoint_last):
+        # 如果路径存在且有内容，则从checkpoint load模型
+        args.model_name_or_path = os.path.join(checkpoint_last, 'pytorch_model.bin')
+        args.config_name = os.path.join(checkpoint_last, 'config.json')
+        idx_file = os.path.join(checkpoint_last, 'idx_file.txt')
+        with open(idx_file, encoding='utf-8') as idxf:
+            args.start_epoch = int(idxf.readlines()[0].strip()) + 1
+
+        step_file = os.path.join(checkpoint_last, 'step_file.txt')
+        if os.path.exists(step_file):
+            with open(step_file, encoding='utf-8') as stepf:
+                args.start_step = int(stepf.readlines()[0].strip())
+        logger.info("reload model from {}, resume from {} epoch".format(checkpoint_last, args.start_epoch))
+
+
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+    config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path,
+                                          cache_dir=args.cache_dir if args.cache_dir else None)
+    config.num_labels=1 # 只有一个label?
+    tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name,
+                                                do_lower_case=False,
+                                                cache_dir=args.cache_dir if args.cache_dir else None)
+    
+    if args.model_name_or_path:
+        model = model_class.from_pretrained(args.model_name_or_path,
+                                            from_tf=bool('.ckpt' in args.model_name_or_path),
+                                            config=config,
+                                            cache_dir=args.cache_dir if args.cache_dir else None)    
+    else:
+        model = model_class(config)
+
+    model = Model(model,config,tokenizer,args)
+
+
+    checkpoint_prefix = 'checkpoint-best-acc/model.bin'
+    output_dir = os.path.join(args.output_dir, '{}'.format(checkpoint_prefix))  
+    model.load_state_dict(torch.load(output_dir))      
+    model.to(args.device)
+
+
+    ## Load CodeBERT (MLM) model
+    codebert_mlm = RobertaForMaskedLM.from_pretrained(args.base_model)
+    tokenizer_mlm = RobertaTokenizer.from_pretrained(args.base_model)
+    codebert_mlm.to('cuda') 
+
+    ## Load Dataset
+    eval_dataset = TextDataset(tokenizer, args,args.eval_data_file)
+
+    # Load original source codes
+    source_codes = []
+    with open(args.eval_data_file) as f:
+        for line in f:
+            js=json.loads(line.strip())
+            code = ' '.join(js['func'].split())
+            source_codes.append(code)
+    assert(len(source_codes) == len(eval_dataset))
+
+    success_attack = 0
+    total_cnt = 0
+
+    recoder = Recorder(args.csv_store_path)
+    
+    attacker = Attacker(args, model, tokenizer, codebert_mlm, tokenizer_mlm, use_bpe=1, threshold_pred_score=0)
+    for index, example in enumerate(eval_dataset):
+        code = source_codes[index]
+        code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words = attacker.greedy_attack(example, code)
+        attack_type = "Greedy"
+        if is_success == -1 and args.use_ga:
+            # 如果不成功，则使用gi_attack
+            code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words = attacker.ga_attack(example, code, initial_replace=replaced_words)
+            attack_type = "GA"
+
+        score_info = ''
+        if names_to_importance_score is not None:
+            for key in names_to_importance_score.keys():
+                score_info += key + ':' + str(names_to_importance_score[key]) + ','
+
+        replace_info = ''
+        if replaced_words is not None:
+            for key in replaced_words.keys():
+                replace_info += key + ':' + replaced_words[key] + ','
+
+        recoder.write(index, code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, score_info, nb_changed_var, nb_changed_pos, replace_info, attack_type)
+        
+        
+        if is_success >= -1 :
+            # 如果原来正确
+            total_cnt += 1
+        if is_success == 1:
+            success_attack += 1
+        
+        if total_cnt == 0:
+            continue
+        print("Success rate: ", 1.0 * success_attack / total_cnt)
+        print("Successful items count: ", success_attack)
+        print("Total count: ", total_cnt)
+        print("Index: ", index)
+        print()
+    
+        
+if __name__ == '__main__':
+    main()
