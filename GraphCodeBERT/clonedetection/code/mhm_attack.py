da5230b1588e15914b687cfbebd2c0f5f8dbec85
jiekeshi <jiekeshi125@gmail.com>
2021-08-22 20:34:30 +0800

    chore(): change _max_iter

--- GraphCodeBERT/clonedetection/code/mhm_attack.py
+++ GraphCodeBERT/clonedetection/code/mhm_attack.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
 
         _res = attacker.mcmc(example, tokenizer, code_pair,
                              _label=ground_truth, _n_candi=30,
-                             _max_iter=50, _prob_threshold=1)
+                             _max_iter=400, _prob_threshold=1)
     
         if _res['succ'] is None:
             continue
