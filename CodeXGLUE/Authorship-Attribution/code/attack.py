dfac2c94cc855e92fe94247848dc9532eb80eca9
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-08-25 15:51:46 +0800

    feat(attack): GA attack

--- CodeXGLUE/Authorship-Attribution/code/attack.py
+++ CodeXGLUE/Authorship-Attribution/code/attack.py
@@ -420,8 +420,8 @@ def main():
                         help="Batch size per GPU/CPU for training.")
     parser.add_argument("--eval_batch_size", default=4, type=int,
                         help="Batch size per GPU/CPU for evaluation.")
-    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.")
+    parser.add_argument("--use_ga", action='store_true',
+                        help="Whether to GA-Attack.")
     parser.add_argument("--learning_rate", default=5e-5, type=float,
                         help="The initial learning rate for Adam.")
     parser.add_argument("--weight_decay", default=0.0, type=float,
@@ -543,6 +543,10 @@ def main():
         # attack(args, example, code, model, tokenizer, codebert_mlm, tokenizer_mlm, use_bpe=1, threshold_pred_score=0)
         
         attack_type = "Greedy"
+        if is_success == -1 and args.use_ga:
+            # 如果不成功，则使用gi_attack
+            code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words = attacker.ga_attack(example, code, initial_replace=replaced_words)
+            attack_type = "GA"
 
 
         score_info = ''
