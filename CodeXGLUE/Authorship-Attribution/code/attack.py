e8664dfec17d7c833b67d3343da76b338afd9e39
jiekeshi <jiekeshi125@gmail.com>
2021-08-26 22:27:44 +0800

    feat(): record query times

--- CodeXGLUE/Authorship-Attribution/code/attack.py
+++ CodeXGLUE/Authorship-Attribution/code/attack.py
@@ -536,7 +536,7 @@ def main():
 
     recoder = Recorder(args.csv_store_path)
     attacker = Attacker(args, model, tokenizer, codebert_mlm, tokenizer_mlm, use_bpe=1, threshold_pred_score=0)
-
+    query_times = 0
     for index, example in enumerate(eval_dataset):
         code = source_codes[index]
         code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, names_to_importance_score, nb_changed_var, nb_changed_pos, replaced_words = attacker.greedy_attack(example, code)
@@ -558,9 +558,10 @@ def main():
         if replaced_words is not None:
             for key in replaced_words.keys():
                 replace_info += key + ':' + replaced_words[key] + ','
-
-        recoder.write(index, code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, score_info, nb_changed_var, nb_changed_pos, replace_info, attack_type)
-        
+        print("Query times in this attack: ", model.query - query_times)
+        print("All Query times: ", model.query)
+        recoder.write(index, code, prog_length, adv_code, true_label, orig_label, temp_label, is_success, variable_names, score_info, nb_changed_var, nb_changed_pos, replace_info, attack_type, model.query - query_times)
+        query_times = model.query
         
         if is_success >= -1 :
             # 如果原来正确
