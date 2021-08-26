16b3b4c8a84d10aab15a0652bb58d02c4dcfa2cf
jiekeshi <jiekeshi125@gmail.com>
2021-08-26 15:12:43 +0800

    feat(): get results

--- GraphCodeBERT/Authorship-Attribution/code/model.py
+++ GraphCodeBERT/Authorship-Attribution/code/model.py
@@ -5,8 +5,10 @@ import torch.nn as nn
 import torch
 from torch.autograd import Variable
 import copy
+import numpy as np
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss, MSELoss
+from torch.utils.data import SequentialSampler, DataLoader
 
 class RobertaClassificationHead(nn.Module):
     """Head for sentence-level classification tasks."""
@@ -60,6 +62,33 @@ class Model(nn.Module):
         else:
             return prob
       
+    def get_results(self, dataset, batch_size):
+        '''Given a dataset, return probabilities and labels.'''
+        eval_sampler = SequentialSampler(dataset)
+        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=batch_size,num_workers=4,pin_memory=False)
+
+        self.eval()
+        logits=[] 
+        labels=[]
+        for batch in eval_dataloader:
+            inputs_ids = batch[0].to("cuda")       
+            attn_mask = batch[1].to("cuda") 
+            position_idx = batch[2].to("cuda") 
+            label=batch[3].to("cuda")  
+            with torch.no_grad():
+                lm_loss,logit = self.forward(inputs_ids, attn_mask, position_idx, label)
+                logits.append(logit.cpu().numpy())
+                labels.append(label.cpu().numpy())
+                
+        logits=np.concatenate(logits,0)
+        labels=np.concatenate(labels,0)
+
+        probs = logits
+        pred_labels = []
+        for logit in logits:
+            pred_labels.append(np.argmax(logit))
+
+        return probs, pred_labels
         
  
         
